642342ae67dc45c4ed7ded95df18e2d74b690bb7
OhmAloneX <mjayvergonio@gmail.com>
2025-09-10 22:19:55 +0800

    Day 01 - Hello, World!

---
+++ Day01_HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine();
+        Console.WriteLine("This is my Day 01 in Learning C_Sharp!");
+        Console.WriteLine();
+    }
+}
