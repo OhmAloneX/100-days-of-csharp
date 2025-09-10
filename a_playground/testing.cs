aa5467d25ef6692e8e0c2c1cc043a035aa0b25cd
OhmAloneX <mjayvergonio@gmail.com>
2025-09-11 00:20:28 +0800

    Update testing.cs

--- a_playground/testing.cs
+++ a_playground/testing.cs
@@ -2,20 +2,17 @@ using System;
 
 namespace Playground
 {
-  class Testing
-  {
-    static void Main(string[]args)
+    class Testing
     {
-      string name = "Jay";
-      int age = 20;
-      string bday = "June";
-      int date_day = "01";
-      int date_year = 2005;
+        static void Main(string[] args)
+        {
+            string name = "Jay";
+            int age = 20;
+            string bday = "June";
+            int date_day = 01;
+            int date_year = 2005;
 
-      Console.WriteLine("My name is M-jay but you can call me " + name + "I am " + age + "years old. " + "My birthday was on " + bday + date_day + date_year.); 
+            Console.WriteLine("My name is M-jay but you can call me " + name + ". I am " + age + " years old. " + "My birthday was on " + bday + " " + date_day + ", " + date_year + ".");
+        }
     }
-  }
 }
-
-
-      
