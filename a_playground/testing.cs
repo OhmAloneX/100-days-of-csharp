89069870ca75c5f9408e42768e100f330e6bdf14
OhmAloneX <mjayvergonio@gmail.com>
2025-09-11 00:03:12 +0800

    Update testing.cs

--- a_playground/testing.cs
+++ a_playground/testing.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System;
+
+namespace Playground
+{
+  class Testing
+  {
+    static void Main(string[]args)
+    {
+      string name = "Jay";
+      int age = 20;
+      string bday = "June";
+      int date_day = "01";
+      int date_year = 2005;
+
+      Console.WriteLine("My name is M-jay but you can call me " + name + "I am " + age + "years old. " + "My birthday was on " + bday + date_day + date_year.); 
+    }
+  }
+}
+
+
+      
